34240186ce867ec7205ae17b5a42a6cad26fee33
xwang <xwang@example.com>
2025-06-24 13:25:39 +0800

    test long context success

--- nsa_utils/dataloader.py
+++ nsa_utils/dataloader.py
@@ -37,62 +37,31 @@ class LongAlignChatTemplateDataset(Dataset):
         return ((input_ids, attn_mask), labels)
 
 
-class LongAlignFlatDataset(Dataset):
-    def __init__(self, tokenizer, max_length=8192):
-        self.raw_data = load_dataset("THUDM/LongAlign-10k", split="train")
+class LongAlignForInferenceDataset(Dataset):
+    def __init__(self, tokenizer, max_length=16384):
         self.tokenizer = tokenizer
         self.max_length = max_length
-        self.samples = self.preprocess()
-
-    def preprocess(self):
-        all_texts = []
-        for example in self.raw_data:
-            messages = example["messages"]
-            # 拼接为一个长文本
-            text = ""
-            for msg in messages:
-                role = msg["role"]
-                content = msg["content"]
-                if role == "user":
-                    text += f"\n### Instruction:\n{content}\n"
-                elif role == "assistant":
-                    text += f"\n### Response:\n{content}\n"
-            all_texts.append(text.strip())
-        return all_texts
+        self.raw_data = load_dataset("THUDM/LongAlign-10k", split="train")
+        
+        self.tokenizer.pad_token = tokenizer.eos_token
 
     def __len__(self):
-        return len(self.samples)
+        return len(self.raw_data)
 
     def __getitem__(self, idx):
-        text = self.samples[idx]
-        encoding = self.tokenizer(
-            text,
-            max_length=self.max_length,
+        example = self.raw_data[idx]
+        messages = example["messages"]
+
+        # 使用 chat template 自动构建 prompt + response 格式
+        encoded = self.tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
             truncation=True,
-            padding="max_length",
+            padding=True,
+            max_length=self.max_length,
             return_tensors="pt"
         )
-        input_ids = encoding["input_ids"].squeeze(0)
-        attention_mask = encoding["attention_mask"].squeeze(0)
 
-        # return {
-        #     "input_ids": input_ids,
-        #     "attention_mask": attention_mask,
-        #     "labels": input_ids.clone()
-        # }
-        return (input_ids, attention_mask, input_ids.clone())
-
-if __name__ == '__main__':
-    model_name = "meta-llama/Llama-3.1-8B-Instruct"
-    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-    tokenizer.pad_token = tokenizer.eos_token  # 必须设置pad_token
-
-    dataset = LongAlignFlatDataset(tokenizer, max_length=8192)
-    dataloader = DataLoader(dataset, batch_size=3, shuffle=True)
-
-    # 查看一个样本
-    for batch in dataloader:
-        print(batch["input_ids"].shape)
-        print(tokenizer.decode(batch["input_ids"][0], skip_special_tokens=True)[:500])
-        break
+        encoded = self.tokenizer(encoded, return_tensors="pt", padding="max_length", max_length=self.max_length, truncation=True)
+        return encoded
 
--- test_minicpm.py
+++ test_minicpm.py
@@ -1,37 +1,27 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
-torch.manual_seed(0)
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from nsa_utils.dataloader import LongAlignForInferenceDataset
 
-path = 'openbmb/MiniCPM4-0.5B'
-device = "cuda"
-tokenizer = AutoTokenizer.from_pretrained(path)
-model = AutoModelForCausalLM.from_pretrained(path, torch_dtype=torch.bfloat16, device_map=device, trust_remote_code=True)
+if __name__ == "__main__":
+	torch.manual_seed(0)
+	path = 'openbmb/MiniCPM4-8B'
+	max_length = 30000
+	device = "cuda"
+	tokenizer = AutoTokenizer.from_pretrained(path)
+	model = AutoModelForCausalLM.from_pretrained(path, torch_dtype=torch.bfloat16, device_map=device, trust_remote_code=True)
 
-# User can directly use the chat interface
-promt = "".join(["Write an article about Artificial Intelligence." for _ in range(5000)])
-responds, history = model.chat(tokenizer, promt, temperature=0.7, top_p=0.7, max_length=40000)
-print(responds)
+	dataset = LongAlignForInferenceDataset(tokenizer=tokenizer, max_length=max_length)
+	
+	for prompt in dataset:
+		prompt = prompt.to(device)
+		outputs = model.generate(**prompt, max_new_tokens=100, do_sample=False, top_p=0.7, temperature=0.0)
+		output_token_ids = [
+			outputs[i][len(prompt[i]):] for i in range(len(prompt['input_ids']))
+		]
 
-# User can also use the generate interface
-# messages = [
-#     {"role": "user", "content": "Write an article about Artificial Intelligence."},
-# ]
-# prompt_text = tokenizer.apply_chat_template(
-#     messages,
-#     tokenize=False,
-#     add_generation_prompt=True,
-# )
-# model_inputs = tokenizer([prompt_text], return_tensors="pt").to(device)
-
-# model_outputs = model.generate(
-#     **model_inputs,
-#     max_new_tokens=1024,
-#     top_p=0.7,
-#     temperature=0.7
-# )
-# output_token_ids = [
-#     model_outputs[i][len(model_inputs[i]):] for i in range(len(model_inputs['input_ids']))
-# ]
-
-# responses = tokenizer.batch_decode(output_token_ids, skip_special_tokens=True)[0]
-# print(responses)
+		responses = tokenizer.batch_decode(output_token_ids, skip_special_tokens=True)[0]
+		print("==============================")
+		print(responses)
+		print("==============================")
+		print(outputs.shape, len(output_token_ids))
+		break
